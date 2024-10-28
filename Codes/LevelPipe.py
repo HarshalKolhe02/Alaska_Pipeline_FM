c96bbcfca0700390c58338a7c65a6a8fa82c8e3f
Harshal <Harshalkolhe0203@gmail.com>
2024-10-28 16:07:31 +0530

    distance changed to km + all files updates

--- Codes/LevelPipe.py
+++ Codes/LevelPipe.py
@@ -16,32 +16,33 @@ FHeadPerMeter=0.002301 # FRICTIONAL HEAD LOSS(m)
 P_inital=68.046  #atm
 
 #defining arrays for storing pipeline length, Pressure values, and Friction losses 
-X=np.arange(0,1287476,5) # ARRAY OF DISTANCES (m)
+X=np.arange(0,1289,2) # ARRAY OF DISTANCES (KM)
 P=np.zeros(X.shape)      # ARRAY OF PRESSURE (m)
 FLoss=np.zeros(X.shape)  # ARRAY OF FRICTIONAL LOSSES (m)
 
 # LOOP FOR CALCULATIONS
 i=0 # Indexing variable
 for d in X :
-    FLoss[i]=RHO*g*FHeadPerMeter*d*PaToAtm #Pascals converted to atm
+    FLoss[i]=RHO*g*FHeadPerMeter*d*PaToAtm*1000 #Pascals converted to atm
     P[i]=P_inital-FLoss[i]
     i+=1
 
-def plain_format(x, pos): #Function to format numbers for graph
-    return f'{x}'  
+# def plain_format(x, pos): #Function to format numbers for graph
+#     return f'{x}'  
 
 # PLOTTING THE GRAPH
    
 plt.plot(X,P,"g")   # Plot the Graphsx
 plt.ylim((-200,80)) # Set Y axis limit
+plt.xlim((0,1400)) # Set X axis limit
 plt.gca().yaxis.set_major_locator(tk.MultipleLocator(20)) #set least count of axis 20
-plt.gca().xaxis.set_major_formatter(tk.FuncFormatter(plain_format)) # format output of x axis to be whole number instead in powers
-plt.xlabel("Pipe Length (meters)")      #setting labels
+plt.gca().xaxis.set_major_locator(tk.MultipleLocator(100))
+plt.xlabel("Pipe Length (Km)")      #setting labels
 plt.ylabel("Pressure Inside Pipe (atm)")#setting labels
 plt.title("PRESSURE INSIDE PIPE WITHOUT USING INLINE PUMP") #setting labels
 plt.show()  # Show the graph
 
 # Sheet export
 data=np.vstack((X,FLoss,P))
-df=pd.DataFrame(data.transpose(),columns=["Pipe Length (meters)","Friction Pressure Loss (atm)","Resultant Pressure (atm)"])
+df=pd.DataFrame(data.transpose(),columns=["Pipe Length (Km)","Friction Pressure Loss (atm)","Resultant Pressure (atm)"])
 df.to_excel("Pressure_Profile_Without_Inline_Pump.xlsx",index=False)
